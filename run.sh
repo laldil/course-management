8ef752ee75c85b30ea21b7b398dd574595e55ca0
Aldiyar <aldiar001122@gmail.com>
2024-06-05 10:00:51 +0500

    FIX: update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p logs
+
 services=("authentication-service" "course-service" "discovery-server" "file-service" "gateway-service" "score-service")
 
 for service in "${services[@]}"
@@ -7,7 +9,13 @@ do
     echo "Starting service: $service"
     cd "$service"
     mvn clean package
-    nohup java -jar target/"$service"-0.0.1-SNAPSHOT.jar &
+    nohup java -jar target/"$service"-0.0.1-SNAPSHOT.jar > "../logs/$service.log" 2>&1 &
+    if [ $? -eq 0 ]; then
+        echo "$service started successfully"
+    else
+        echo "Failed to start $service"
+        exit 1
+    fi
     cd ..
 done
 
